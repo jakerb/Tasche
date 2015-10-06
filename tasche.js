510cfb432dcd40a2a7f110bc50fff51585aa71b3
Jake Bown <jakebown@gmail.com>
2015-10-06 17:29:08 +0100

    Complete redesign
    Tasche now works better at doing what it does best, you only need to call render.

--- tasche.js
+++ tasche.js
@@ -4,42 +4,40 @@
  * Feel free to drop me a line at jake@jakebown.com
  */
 
-$.fn.tasche = function (tasche) {
-	var page;
-	if(!typeof(tasche.template) === 'undefined') { 
-		$.ajax({ url: tasche.template, type: "GET", async: false, success: function(reply) {
-			if(reply != null && reply.length > 1) {
-				render(reply);
-			}
-			else {
-				render($(this).html());
-			}
-		}});
-	}
-	else {
-		render($(this).html());
-	}
+var tasche = {
 
-		$(this).html(page);
+	render:function(tasche, callback) {
+		var page;
+		var that = this;
+		$.ajax({
+		  url: tasche.template,
+		  success: function(html) {
+		  	that.process(html, tasche);
+		  	if(callback) {
+				callback();
+			}
+		  },
+		  dataType: "html"
+		});
 
-	
-	function render(val) {
+	},
+
+	process:function(val, tasche) {
 		find = val.match(/([^}{]+)(?=})/g);
 		var y = val;
+
 	    if (find != null && find.length > 0) { 
 			for (var i = find.length - 1; i >= 0; i--) {
 				var x = find[i];
 				var z = tasche[x];
-				console.log("{{"+x+"}}");
 				var y = y.replace("{{"+x+"}}", z);
 			};
 			page = y;
 		}
 		else {
-			console.log("Tasche Error.");
+			page = y;
 		}
-	}
 
-	
-	
-};
+		$(tasche.element).html($('body').find(tasche.element).html() + page);
+	}
+}
